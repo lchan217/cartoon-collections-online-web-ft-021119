c7adfa56056257f27309f0f324ffa0f1f305a8f7
Lizzie Chan <lchan217@gmail.com>
2019-03-06 02:19:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,33 +1,23 @@
-require 'pry'
+require "pry"
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index do |dwarf, index|
-    puts"#{index+1}. #{dwarf}"
-  end 
+  dwarves.each_with_index do |name, i|
+    puts "#{i+1} #{name}"
+  end
 end 
 
 def summon_captain_planet(array)
-  array.map do |thing|
-    "#{thing.capitalize!}!"
-  end 
+  array.map do |call|
+    "#{call.capitalize}!"
+  end
 end 
 
 def long_planeteer_calls(array)
-  array.each do|call|
-    if call.length > 3 
-      return false 
-    else 
-      return true 
-    end
-  end 
+  array.any? {|element| element.length > 4}
 end 
+
 def find_the_cheese(array)
-  
-  if array.include?("cheddar")
-    "cheddar"
-    elsif array.include?("gouda")
-    "gouda"
-  elsif array.include?("camembert")
-  "camembert"
-else 
-end 
+  cheeses = ["cheddar", "gouda", "camembert"]
+  array.find do |element|
+    cheeses.include?(element)
+  end
 end 
